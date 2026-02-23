66c6203dc3394252a4c31f039dda24633e2bba81
GSD Agent <gsd-agent@delogo.it>
2026-02-23 22:14:30 +0100

    fix bug e performance

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,27 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwind from '@tailwindcss/vite';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import tailwind from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://delogo.it',
-    integrations: [sitemap()],
-    vite: {
-        plugins: [tailwind()],
+  site: "https://delogo.it",
+  integrations: [sitemap()],
+  build: {
+    inlineStylesheets: "always",
+  },
+  vite: {
+    plugins: [tailwind()],
+  },
+  image: {
+    domains: ["images.unsplash.com"],
+    remotePatterns: [{ protocol: "https", hostname: "images.unsplash.com" }],
+  },
+  i18n: {
+    defaultLocale: "it",
+    locales: ["it", "en"],
+    routing: {
+      prefixDefaultLocale: false,
     },
-    image: {
-        domains: ['images.unsplash.com'],
-        remotePatterns: [{ protocol: 'https', hostname: 'images.unsplash.com' }],
-    },
-    i18n: {
-        defaultLocale: "it",
-        locales: ["it", "en"],
-        routing: {
-            prefixDefaultLocale: false
-        }
-    },
+  },
 });
